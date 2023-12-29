62f2cdf8b20cd41f9ef9584fd28bfa091c1f2e11
klsharma22 <30960037+klsharma22@users.noreply.github.com>
2023-12-29 12:36:58 +0530

    GPU devices has been set

--- layer_normalization.py
+++ layer_normalization.py
@@ -2,18 +2,19 @@ import torch
 import math
 from torch import nn
 import torch.nn.functional as F
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class LayerNormalization(nn.Module):
     def __init__(self, parameters_shape):
         super().__init__()
         self.parameters_shape=parameters_shape
-        self.norm = nn.LayerNorm(self.parameters_shape).to(device='cuda:0')
+        self.norm = nn.LayerNorm(self.parameters_shape).to(device)
 
     def forward(self, inputs):
-        inputs = inputs.to(device='cuda:0')
+        inputs = inputs.to(device)
         out = self.norm(inputs)
-        out = out.to(device='cpu')
+        out = out.to(device)
         torch.cuda.empty_cache()
         print(f"Layer normalization done , Size=: {out.size()}",'\n')
         return out
