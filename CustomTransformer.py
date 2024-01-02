6fa0805739f59a10bd6ef3192bf37b48c2055d1e
klsharma22 <30960037+klsharma22@users.noreply.github.com>
2024-01-03 00:56:21 +0530

    created a file which has the embedding of first sentence of each language for experiment

--- CustomTransformer.py
+++ CustomTransformer.py
@@ -203,12 +203,12 @@ class Transformer(nn.Module):
 # transformer_model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx)
 
 device = torch. device("cuda" if torch.cuda.is_available() else "cpu")
-x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
-trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
+x = torch.tensor([[]]).to(device)
+trg = torch.tensor([[]]).to(device)
 src_pad_idx = 0
 trg_pad_idx = 0
-src_vocab_size = 10
-trg_vocab_size = 10
+src_vocab_size = 500
+trg_vocab_size = 500
 model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx).to(device)
 out = model(x, trg)
 print(out)
